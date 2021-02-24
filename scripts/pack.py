5c905c99d28d2242e2874f26dfa42407dba8cccd
Pamela <aomeidev@gmail.com>
2021-02-25 00:21:26 +0800

    build: fill verison in installer

--- scripts/pack.py
+++ scripts/pack.py
@@ -6,11 +6,12 @@ import zipfile
 from pathlib import Path
 
 CUR_DIR = Path(__file__).parent
+INSTALLER_PY = 'installer.py.exe'
 APP_ZIP = 'app.zip'
 VERSION_FILE = 'data/html/version.json'
 
 
-def write_verison(ver):
+def write_verison_file(ver):
     ver_data = json.dumps(ver, indent=4).encode()
 
     with zipfile.ZipFile(APP_ZIP, 'a') as zf:
@@ -21,6 +22,40 @@ def write_verison(ver):
         f.write(ver_data)
 
 
+def u8to16(u8):
+    return b''.join([b'%c\0' % x for x in u8])
+
+
+def fill_installer_ver(installer_ver):
+    TPL_BUF_SIZE = 260
+    assert len(installer_ver) < TPL_BUF_SIZE
+    BLOCK_SIZE = 100 * 1024
+    template = u8to16(b'{{app_version}}')
+    padding = len(template) - 1
+    offset = 0
+
+    def overwrite(index):
+        f.seek(index)
+        f.write(b'\0' * TPL_BUF_SIZE)
+        f.seek(index)
+        ver_data = u8to16(installer_ver.encode())
+        f.write(ver_data)
+
+    with open(INSTALLER_PY, 'rb+') as f:
+        while True:
+            f.seek(offset)
+            block = f.read(BLOCK_SIZE + padding)
+            if not block:
+                return False
+
+            index = block.find(template)
+            if index != -1:
+                overwrite(offset + index)
+                return True
+
+            offset += len(block)
+
+
 def main():
     now = datetime.datetime.now()
     build_tag = now.strftime("%Y%m%d_%H%M")[2:]
@@ -28,14 +63,17 @@ def main():
     manifest = read_manifest()
     name = manifest['name']
     major_ver = manifest['version']
-    write_verison({
+    write_verison_file({
         'name': name,
         'verison': major_ver,
         'build': build_tag,
     })
 
     file_name = f'{name}-{major_ver}-{build_tag}.exe'
-    cmd = f'installer.py.exe push {APP_ZIP} {file_name}'
+    installer_ver = f' ({major_ver}-{build_tag})'
+    assert fill_installer_ver(installer_ver)
+
+    cmd = f'{INSTALLER_PY} push {APP_ZIP} {file_name}'
     exit(os.system(cmd))
 
 
